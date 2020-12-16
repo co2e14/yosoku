c2f4ab8e026a2a0e74a8f57460857a3d912aaf8a
co2e14 <christian.orr@diamond.ac.uk>
2020-12-16 16:35:54 +0000

    working

--- yosoku.py
+++ yosoku.py
@@ -8,10 +8,17 @@ ms = miller.build_set(
     crystal_symmetry=cctbx.crystal.symmetry(
         space_group_symbol="P321", unit_cell=(150, 150, 45, 90, 90, 120)
     ),
-    anomalous_flag=False,
-    d_min=1.8,
+    anomalous_flag=True,
+    d_min=1.9,
 )
 print(ms.size())
-
 #print(list(ms.indices()))
 print(ms.d_max_min())
+print(ms.crystal_symmetry())
+print(ms.minimum_wavelength_based_on_d_min())
+print(ms.generate_bivoet_mates())
+#print(ms.show_completeness(2))
+print(ms.minimum_wavelength_based_on_d_min())
+print(ms.minimum_wavelength_based_on_d_min())
+print(dir(ms))
+ms.miller_indices_as_pdb_file(file_name='mil.pdb')
