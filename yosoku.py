20dbe2d84a1f00ea086ff56311bdeeacf528e2f9
Chris Orr <christian.orr@diamond.ac.uk>
2020-12-17 18:12:35 +0000

    working with test evn

--- yosoku.py
+++ yosoku.py
@@ -1,8 +1,6 @@
 import cctbx
 from cctbx import miller
-
-
-# Aim is to make a web interface to put crystal details in and give a likely outcome of a project on I23.
+from iotbx.pdb import hierarchy
 
 ms = miller.build_set(
     crystal_symmetry=cctbx.crystal.symmetry(
@@ -11,14 +9,29 @@ ms = miller.build_set(
     anomalous_flag=True,
     d_min=1.9,
 )
+
+refl = int(ms.size())
 print(ms.size())
 #print(list(ms.indices()))
 print(ms.d_max_min())
 print(ms.crystal_symmetry())
-print(ms.minimum_wavelength_based_on_d_min())
 print(ms.generate_bivoet_mates())
-#print(ms.show_completeness(2))
-print(ms.minimum_wavelength_based_on_d_min())
-print(ms.minimum_wavelength_based_on_d_min())
-print(dir(ms))
-ms.miller_indices_as_pdb_file(file_name='mil.pdb')
+#print(dir(ms))
+#ms.miller_indices_as_pdb_file(file_name='mil.pdb')
+#msa = miller.array(ms)
+sgs = 0
+pdb_in = hierarchy.input(file_name="6fax.pdb")
+for chain in pdb_in.hierarchy.only_model().chains():
+  for residue_group in chain.residue_groups():
+    for atom_group in residue_group.atom_groups():
+      for atom in atom_group.atoms():
+        if (atom.element.strip().upper() == "S"):
+            sgs += 1
+print(sgs)
+print(refl/sgs)
+
+#pdb_atoms = pdb_in.hierarchy.atoms()
+#sel_cache = pdb_in.hierarchy.atom_selection_cache()
+#c_alpha_sel = sel_cache.selection("name sg") # XXX not case sensitive!
+#c_alpha_atoms = pdb_atoms.select(c_alpha_sel)
+#print(len(c_alpha_atoms))
