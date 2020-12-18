11a59740d7557e36742b3a22851725f45d2b04b0
Chris Orr <christian.orr@diamond.ac.uk>
2020-12-18 12:17:23 +0000

    working as is

--- yosoku.py
+++ yosoku.py
@@ -2,36 +2,55 @@ import cctbx
 from cctbx import miller
 from iotbx.pdb import hierarchy
 
+sg_in = input("Space Group (X123): ")
+uc_in = input("Unit Cell (a, b, c, al, be, ga): ")
+asu_mol = int(input("Number of molecules in the ASU: "))
+d_min_in = float(input("High res: "))
+s_type = input("Supply (p)db file, (s)equence, or (n)umber of scatterers: ")
+
+if s_type == "p":
+    pdb_file = str(input("Path to PDB file: "))
+    s = 0
+    pdb_in = hierarchy.input(file_name="6fax.pdb")
+    for chain in pdb_in.hierarchy.only_model().chains():
+        for residue_group in chain.residue_groups():
+            for atom_group in residue_group.atom_groups():
+                for atom in atom_group.atoms():
+                    if atom.element.strip().upper() == "S":
+                        s += 1
+if s_type == "n":
+    s = int(input("Number of S atoms: "))
+if s_type == "s":
+    s = str(input("Sequence (letters only): ")).count("C" or "M")
+
 ms = miller.build_set(
     crystal_symmetry=cctbx.crystal.symmetry(
-        space_group_symbol="P321", unit_cell=(150, 150, 45, 90, 90, 120)
+        space_group_symbol=sg_in, unit_cell=(uc_in)
     ),
     anomalous_flag=True,
-    d_min=1.9,
+    d_min=d_min_in,
 )
-
 refl = int(ms.size())
-print(ms.size())
-#print(list(ms.indices()))
-print(ms.d_max_min())
-print(ms.crystal_symmetry())
-print(ms.generate_bivoet_mates())
-#print(dir(ms))
-#ms.miller_indices_as_pdb_file(file_name='mil.pdb')
-#msa = miller.array(ms)
-sgs = 0
-pdb_in = hierarchy.input(file_name="6fax.pdb")
-for chain in pdb_in.hierarchy.only_model().chains():
-  for residue_group in chain.residue_groups():
-    for atom_group in residue_group.atom_groups():
-      for atom in atom_group.atoms():
-        if (atom.element.strip().upper() == "S"):
-            sgs += 1
-print(sgs)
-print(refl/sgs)
+ref_per_s = refl / (s * asu_mol)
 
-#pdb_atoms = pdb_in.hierarchy.atoms()
-#sel_cache = pdb_in.hierarchy.atom_selection_cache()
-#c_alpha_sel = sel_cache.selection("name sg") # XXX not case sensitive!
-#c_alpha_atoms = pdb_atoms.select(c_alpha_sel)
-#print(len(c_alpha_atoms))
+print(
+    "\nThere are", s, "sulphur atoms in your protein and", (s * asu_mol), "in the ASU"
+)
+print("The number of reflections per sulphur atom is", ref_per_s)
+
+if ref_per_s == 0:
+    print("\nSomething went wrong...")
+if 0 < ref_per_s < 500:
+    print("\nPhasing is highly unlikely to succeed with this crystal")
+if 500 <= ref_per_s < 800:
+    print("\nPhasing is unlikely to succeed with this crystal")
+if 800 <= ref_per_s < 1100:
+    print(
+        "\nPhasing is possible to succeed with this crystal, though will be a borderline case"
+    )
+if 1100 <= ref_per_s < 2000:
+    print("\nPhasing is likely to succeed with this crystal")
+if 2000 <= ref_per_s < 10000:
+    print("\nPhasing is highly to succeed with this crystal")
+if 10000 <= ref_per_s:
+    print("\nPhasing is essentially guaranteed to succeed with this crystal")
