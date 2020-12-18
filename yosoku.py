6d4cf835159b2703d193ad397acbd6225cd95413
Chris Orr <christian.orr@diamond.ac.uk>
2020-12-18 13:40:07 +0000

    neaten up and typo

--- yosoku.py
+++ yosoku.py
@@ -2,6 +2,8 @@ import cctbx
 from cctbx import miller
 from iotbx.pdb import hierarchy
 
+print("\n***** Prediction of SAD Phasing on I23 *****")
+
 sg_in = input("Space Group (X123): ")
 uc_in = input("Unit Cell (a, b, c, al, be, ga): ")
 asu_mol = int(input("Number of molecules in the ASU: "))
@@ -33,28 +35,33 @@ ms = miller.build_set(
 refl = int(ms.size())
 ref_per_s = refl / (s * asu_mol)
 
-print("\n***** RESULT *****\n")
+print("\n***** RESULT *****")
 print(
     "\nThere are", s, "sulphur atoms in your protein and", (s * asu_mol), "in the ASU"
 )
 print("The number of reflections per sulphur atom is", ref_per_s)
 
+email = "\nTo receive sample mounts and arrage beamtime (via BAG or rapid access), please email i23@diamond.ac.uk"
+
 if ref_per_s == 0:
     print("\nSomething went wrong...")
 if 0 < ref_per_s < 500:
     print("\nPhasing is highly unlikely to succeed with this crystal")
+    print(
+        "\nIf you would like to discuss phasing alternatives, please email i23@diamond.ac.uk"
+    )
 if 500 <= ref_per_s < 800:
     print("\nPhasing is unlikely to succeed with this crystal")
+    print("\nIf you would like to discuss this project, please email i23@diamond.ac.uk")
 if 800 <= ref_per_s < 1100:
-    print(
-        "\nPhasing is possible with this crystal, though will be a borderline case"
-    )
+    print("\nPhasing is possible with this crystal, though will be a borderline case")
+    print(email)
 if 1100 <= ref_per_s < 2000:
     print("\nPhasing is likely to succeed with this crystal")
+    print(email)
 if 2000 <= ref_per_s < 10000:
     print("\nPhasing is highly to succeed with this crystal")
+    print(email)
 if 10000 <= ref_per_s:
     print("\nPhasing is essentially guaranteed to succeed with this crystal")
-
-if 800 <= ref_per_s:
-    ("To receive sample mount and arrage beamtime (via BAGG or rapid access), please email i23@diamond.ac.uk")
+    print(email)
