b063f3e9585737fa92cef79bf53f3d9511a983ef
Chris Orr <christian.orr@diamond.ac.uk>
2021-01-04 14:02:40 +0000

    include graph

--- yosoku.py
+++ yosoku.py
@@ -1,6 +1,18 @@
 import cctbx
 from cctbx import miller
 from iotbx.pdb import hierarchy
+import decimal
+import matplotlib.pyplot as plt
+
+
+# import os, libtbx.env_config
+# import libtbx.load_env
+
+# print(os.getenv("LIBTBX_BUILD"))
+# print("Env:", repr(os.getenv("LIBTBX_BUILD")))
+# print(libtbx.env_config.get_installed_path())
+
+# if above gives error like '', run "unset LIBTBX_BUILD" in terminal
 
 
 class colours:
@@ -14,28 +26,33 @@ class colours:
 
 print("\n***** Prediction of SAD Phasing on I23 *****\n")
 
-sg_in = input("Space Group (X123): ")
-uc_in = input("Unit Cell (a, b, c, al, be, ga): ")
-asu_mol = int(input("Number of molecules in the ASU: "))
-d_min_in = float(input("High res: "))
-s_type = input("Supply (p)db file, (s)equence, or (n)umber of scatterers: ")
+# sg_in = input("Space Group (X123): ")
+# uc_in = input("Unit Cell (a, b, c, al, be, ga): ")
+# asu_mol = int(input("Number of molecules in the ASU: "))
+# d_min_in = float(input("High res: "))
+# s_type = input("Supply (p)db file, (s)equence, or (n)umber of scatterers: ")
 
-if s_type == "p":
-    pdb_file = str(input("Path to PDB file: "))
-    s = 0
-    pdb_in = hierarchy.input(file_name="6fax.pdb")
-    for chain in pdb_in.hierarchy.only_model().chains():
-        for residue_group in chain.residue_groups():
-            for atom_group in residue_group.atom_groups():
-                for atom in atom_group.atoms():
-                    if atom.element.strip().upper() == "S":
-                        s += 1
-if s_type == "n":
-    s = int(input("Number of S atoms: "))
-if s_type == "s":
-    s_in = str((input("Sequence (letters only): ")).replace(" ", ""))
-    s = s_in.count("C") + s_in.count("M")
+sg_in = "P321"
+uc_in = "150 150 45 90 90 120"
+asu_mol = 2
+d_min_in = 2.3
+s = 14
 
+# if s_type == "p":
+#     pdb_file = str(input("Path to PDB file: "))
+#     s = 0
+#     pdb_in = hierarchy.input(file_name="6fax.pdb")
+#     for chain in pdb_in.hierarchy.only_model().chains():
+#         for residue_group in chain.residue_groups():
+#             for atom_group in residue_group.atom_groups():
+#                 for atom in atom_group.atoms():
+#                     if atom.element.strip().upper() == "S":
+#                         s += 1
+# if s_type == "n":
+#     s = int(input("Number of S atoms: "))
+# if s_type == "s":
+#     s_in = str((input("Sequence (letters only): ")).replace(" ", ""))
+#     s = s_in.count("C") + s_in.count("M")
 
 def predict(sg_in, uc_in, asu_mol, d_min, s):
     ms = miller.build_set(
@@ -43,7 +60,7 @@ def predict(sg_in, uc_in, asu_mol, d_min, s):
             space_group_symbol=sg_in, unit_cell=(uc_in)
         ),
         anomalous_flag=True,
-        d_min=d_min_in,
+        d_min=d_min,
     )
     refl = int(ms.size())
     ref_per_s = refl / (s * asu_mol)
@@ -96,3 +113,14 @@ if 10000 <= ref_per_s:
         f"{colours.OKGREEN}\nPhasing is essentially guaranteed to succeed with this crystal{colours.ENDC}"
     )
     print(email)
+
+res_v_refl = []
+for high_lim in [x / 10.0 for x in range(8, 46, 1)]:
+    ref_per_s = predict(sg_in, uc_in, asu_mol, high_lim, s)
+    res_v_refl += [(high_lim, ref_per_s)]
+    print(high_lim, ref_per_s)
+
+print(res_v_refl)
+
+plt.scatter(*zip(*res_v_refl))
+plt.show()
